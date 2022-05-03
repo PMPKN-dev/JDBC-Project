6950112e600c098ae3241425b704626dd642909f
CVHSguy <101105190+CVHSguy@users.noreply.github.com>
2022-05-03 10:00:52 +0200

    it verks

--- src/Application/Controller/CreateUser.java
+++ src/Application/Controller/CreateUser.java
@@ -23,12 +23,14 @@ static Connection con;
     }
     @FXML
     public void onCreateUserClick() throws SQLException {
-
         Connection con = DBCon.getCon();
-        PreparedStatement p = con.prepareStatement("insert into tbl_Customer values (?,?,?)");
+        PreparedStatement p = con.prepareStatement("insert into tbl_Customer values (?,?,?,?)");
         p.setString(1,Name.getText());
         p.setString(2,Address.getText());
-        p.setString(3,Phone.getText());
+        p.setInt(3, Integer.parseInt(Phone.getText()));
+        p.setInt(4,0);
+        p.executeUpdate();
+        p.close();
         DBCon.closeCon();
     }
 
