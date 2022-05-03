eea8a4851cb73bb080a4a569755c3fcddf4beff1
PMPKN-dev <michal-janczak@outlook.com>
2022-05-03 09:20:37 +0200

    lolmao

--- src/Application/Controller/CreateUser.java
+++ src/Application/Controller/CreateUser.java
@@ -1,13 +1,23 @@
 package Application.Controller;
 
 import Application.ControllerNames;
+import Application.DBCon;
 import javafx.fxml.FXML;
+
+import java.sql.Connection;
 
 public class CreateUser {
 
     @FXML
     public void onBackClick(){
         Main.changeScene(ControllerNames.Registration);
+    }
+
+
+    public void onCreteUserClick(){
+        Connection con = DBCon.getCon();
+        //magic
+        DBCon.closeCon();
     }
 
 }
